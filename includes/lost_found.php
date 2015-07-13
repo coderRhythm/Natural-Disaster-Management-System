5b679bbaf8ff62e1e86399fa2f98b2c9bd9849bb
Krati <tripathi.krati@gmail.com>
2015-07-13 01:09:42 +0100

    Include files and CSS file added
    CSS file, JS file and included files like header, footer etc. added.

---
+++ includes/lost_found.php
@@ -0,0 +1,63 @@
+<?php require_once("includes/ses.php"); ?>
+<?php require_once("includes/header.php"); ?>
+<?php require_once("includes/nav.php"); ?>
+
+        	<div class="cleaner"></div>
+            <div class="box">
+            	<h3><span></span>News</h3>
+
+                <img src="images/nd1.jpg" alt="image" />
+                <div class="news_title"><a href="#"> Mudslides Explained: Behind the Washington State Disaster</a></div>
+                <p>A fatal mudslide in rural northwestern Washington State over the weekend underscores the dangers of this fast-moving natural hazard.</p>
+		<div class="cleaner"></div>
+		<img src="images/nd2.jpg"/>
+		<div class="news_title"><a href="http://www.huffingtonpost.com/2014/03/26/extreme-weather-natural-disasters-insurance-losses_n_5033351.html">Extreme Weather, Natural Disasters Led To $45 Billion In Insurance Losses In 2013</a></div>
+		<p>GENEVA- Disasters such as floods in Europe, winter storms in the U.S. and typhoons in Asia cost insurance companies $45 billion in 2013, a leading Swiss firm said Wednesday.</p>
+
+          </div>
+
+      </div> <!-- end of side column -->
+
+        <div id="templatemo_main_column">
+
+	<a href="home.php"><div id="disp_name">
+	    <?php echo $val;?>
+	</div></a>
+            <div class="main_column_box">
+            
+            <div class="cleaner_h80"></div>
+            
+                <h3 id="topic1"><span></span>Welcome to NDMS!</h3>
+
+              <img id="image1" class="image_wrapper fl_image" src="images/nd3.jpg" alt="image" />
+              <p class="em_text" id="desc1">A natural disaster is a major adverse event resulting from natural processes of the Earth; examples include floods, volcanic eruptions, earthquakes, tsunamis, and other geologic processes. A natural disaster can cause loss of life or property damage, and typically leaves some economic damage in its wake, the severity of which depends on the affected population's resilience, or ability to recover.</p>
+
+                <div class="cleaner"></div>
+            </div>
+          <div class="main_column_box" id="topic_head">
+                <h3 id="topic2"><span></span>Our Services</h3>
+            <img id="image3" class="image_wrapper fr_image" src="images/nd4.jpg" alt="image" />
+                <p id="desc2">The NDMS provides its users with information on how to survive a Natural Disaster. We want the people out there to know the dangers of Natural Disasters and to understand how important it is to be prepared incase of a Natural Disaster.</p>
+
+            <div class="cleaner_h30"></div>
+
+				<ul class="list_02" id="list1">
+                	<li>We provide interactive videos which depict every step needed to take in time of a disaster.</li>
+                    <li>We provide a descriptive account of those people who alreadt witnessed a disaster and how they survived it.</li>
+                    <li>We provide a Lost and Found Functionality for those people who went missing in a recent event.</li>
+                    <li>We provide a descriptive overview of every disaster known.</li>
+                    <li>We provide maps.</li>
+                </ul>
+            <div class="cleaner_h30"></div>
+                <div class="button_01"><a href="#">Read more</a></div>
+
+          </div>
+<img id="image2" src="" style="width: 100%;"/>
+        </div>
+
+        <div class="cleaner"></div>
+        <div id="top"></div>
+        <div id="bottom"></div>
+    </div>
+
+<?php require("includes/footer.php"); ?>
